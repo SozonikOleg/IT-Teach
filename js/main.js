d3fefcaa4ff37fe7791a6c9538cd790d271a3453
OlegSozonik <sozonik999@gmail.com>
2018-08-03 18:17:34 +0300

    responsive_mobile3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav',
-        infinite: true
-    });
+$(function(){
 
-    $('.slider-nav').slick({
-        slidesToShow: 3,
-        asNavFor: '.slider-for',
-        focusOnSelect: true,
-        infinite: true,
-        arrows: false
-    });
+    $('.menu__toggle').on('click', function(){
+
+        $('.nav').slideToggle(300, function(){
+
+            if ($(this).css('display')==='none'){
+                $(this).removeAttr('style');
+            }
+
+        });
+
+    })
+
+    $('.menu__toggle').on('click', function () {
+        $('.nav').addClass('active');
+      });
+
+    $('.icon__close').on('click', function () {
+        $('.nav').removeClass('active');
+    });  
+    $('.icon__close').on('click', function () {
+        $('.nav').removeAttr('style');
+    }); 
+
 });
+
